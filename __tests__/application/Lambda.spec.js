129c4375ec947123129e74ebc6aceaaed3d5197d
Tolentino <carlos.tolentinoe@gmail.com>
2025-04-10 14:18:35 -0400

    Add test Lambda

--- __tests__/application/Lambda.spec.js
+++ __tests__/application/Lambda.spec.js
@@ -1,10 +1,11 @@
-import { describe, test, expect, beforeEach, jest } from '@jest/globals';
+import {describe, test, expect, beforeEach, jest} from '@jest/globals';
 
 import createLambdaHandler from '../../application/Lambda.js';
-import { EventLambda } from '../../domain/EventLambda.js';
-import { ResponseLambda } from '../../domain/Response.js';
+import {EventLambda} from '../../domain/EventLambda.js';
+import {ResponseLambda} from '../../domain/Response.js';
 import {EventDataBuilder} from "../../__mocks__/EventDataBuilder.js";
 import {SellerDataBuilder} from "../../__mocks__/SellerDataBuilder.js";
+import {HeaderLambda} from "../../domain/HeaderLambda.js";
 
 describe('createLambdaHandler', () => {
     let serviceLocatorMock;
@@ -22,7 +23,7 @@ describe('createLambdaHandler', () => {
         const eventData = new EventDataBuilder()
             .withHeaders({
                 Authorization: 'token123',
-                b2bSession: { Authorization: 'b2bToken456' },
+                b2bSession: {Authorization: 'b2bToken456'},
                 cpgId: 'CPG-001',
                 countryId: 'CO',
                 organizationId: 'ORG-123',
@@ -39,7 +40,7 @@ describe('createLambdaHandler', () => {
             .withDeliveryFrozenDate('2025-12-31')
             .withPaymentMethod('CREDIT')
             .build();
-        const mockSeller =  new SellerDataBuilder().build();
+        const mockSeller = new SellerDataBuilder().build();
         serviceLocatorMock.getSellerByEmail.mockResolvedValue(mockSeller);
 
         const response = await handler.handleRequest(eventData);
@@ -51,11 +52,48 @@ describe('createLambdaHandler', () => {
         expect(response.error).toEqual(null);
     });
 
+    test('should return an authorization error when the headers are not set appropriately.', () => {
+        const badEvent = {
+            headers: {
+                authorization: "",
+                cpgId: "001",
+                countryId: "CL",
+                organizationId: "3043",
+                transactionId: "",
+                b2bSession: {
+                    Authorization: ""
+                },
+                appID: "MiMarket",
+                appVersion: "1.20.8",
+                appOs: "android",
+                appOsVersion: "33"
+            },
+            payload: {
+                clientId: "46715",
+                paymentMethod: "[{\"segmentId\":1,\"paymentMethod\":\"E\"}]",
+                deliveryDate: "2025-03-19T23:59:00.000Z",
+                deliveryFrozenDate: "",
+                latitude: 6.2117653,
+                longitude: -75.6026481,
+                orderId: "10013470"
+            }
+        }
+
+        expect(() => {
+            new HeaderLambda(badEvent.headers);
+        }).toThrow(
+            new Error(JSON.stringify({
+                code: 'UNAUTHORIZED',
+                message: 'Unauthorized',
+            }, null, 2))
+        );
+    });
+
     test('should handle errors and return 500 code', async () => {
         const eventData = new EventDataBuilder()
             .withHeaders({
                 Authorization: 'token123',
-                b2bSession: { Authorization: 'b2bToken456' },
+                b2bSession: {Authorization: 'b2bToken456'},
                 cpgId: 'CPG-001',
                 countryId: 'CO',
                 organizationId: 'ORG-123',
@@ -81,6 +119,6 @@ describe('createLambdaHandler', () => {
         expect(response).toBeInstanceOf(ResponseLambda);
         expect(response.statusCode).toBe(500);
         expect(response.data).toBeNull();
-        expect(response.error).toEqual(JSON.stringify({ error: errorMessage }));
+        expect(response.error).toEqual(JSON.stringify({error: errorMessage}));
     });
 });
